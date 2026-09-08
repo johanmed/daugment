fb95ad3ce73c49b37232c336374d240dab524191
Johannes Medagbe <johanmedagbe@gmail.com>
2026-09-08 17:29:09 +0300

    New: move from script arguments to environment variables

--- augment.py
+++ augment.py
@@ -1,13 +1,16 @@
 """
 Main script of the package
-To run: python augment.py --dataset-path <> --question-field <> --answer-field <> --local-dataset <> --output-path <>
+To run: python augment.py
 Author: Johannes Medagbe
 Copyright (c) 2026
 """
 
 import argparse
+import os
 
+import dspy
 import pandas as pd
+from dotenv import load_dotenv
 
 from daugment.inference.embedding.extraction import extract
 from daugment.inference.text.recommendation import recommend
@@ -15,38 +18,43 @@ from daugment.transforms.computation import propose_terms
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dataset-path", help="Path to dataset file or id")
-    parser.add_argument("--output-path", help="Path to save augmented dataset at")
-    parser.add_argument("--question-field", help="Question field in the dataset")
-    parser.add_argument("--answer-field", help="Answer field in the dataset")
     parser.add_argument(
-        "--local-dataset",
-        default=False,
-        help="Differentiate between local and HuggingFace dataset",
-    )
-    parser.add_argument(
-        "--repo-word-model",
-        default="adameubanks/YearlyWord2Vec",
-        help="Path to repository offering word2vec model on HuggingFace Hub",
-    )
-    parser.add_argument(
-        "--word-model-name",
-        default="word2vec-2025/word2vec_2025.model",
-        help="Specific version of word2vec model in the repository",
+        "--env-file", default=".env", help="Path to file with environment file"
     )
     args = parser.parse_args()
 
+    load_dotenv(dotenv_path=args.env_file)
+    dataset_path = os.environ["DATASET_PATH"]
+    question_field = os.environ["QUESTION_FIELD"]
+    answer_field = os.environ["ANSWER_FIELD"]
+    local_dataset = bool(int(os.environ["LOCAL_DATASET"]))
+    output_path = os.environ["OUTPUT_PATH"]
+    repo_word_model = os.environ["REPO_WORD_MODEL"]
+    word_model_name = os.environ["WORD_MODEL_NAME"]
+    llm_name = os.environ["LLM_NAME"]
+    api_key = os.environ["API_KEY"]
+
+    llm = dspy.LM(
+        llm_name,
+        api_key=api_key,
+        max_tokens=5_000,
+        temperature=1,
+        cache=False,
+        verbose=False,
+    )
+    dspy.configure(lm=llm)
+
     questions, unpacked_terms, answers, embeddings = extract(
-        args.dataset_path,
-        args.question_field,
-        args.answer_field,
-        args.repo_word_model,
-        args.word_model_name,
-        args.local_dataset,
+        dataset_path,
+        question_field,
+        answer_field,
+        repo_word_model,
+        word_model_name,
+        local_dataset,
     )
     new_terms = propose_terms(unpacked_terms, embeddings)
     new_dataset = recommend(questions, new_terms, answers)
     old_dataset = dict(zip(questions, answers))
     final_dataset = {**old_dataset, **new_dataset}
     df = pd.DataFrame(list(final_dataset.items()), columns=["question", "answer"])
-    df.to_csv(args.output_path, header=True)
+    df.to_csv(output_path, header=True)
