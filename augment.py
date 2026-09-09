217566e2076c5af25a55b44f8c22691c0fe0d704
Johannes Medagbe <johanmedagbe@gmail.com>
2026-09-09 10:33:30 +0300

    Fix: condition run on non-existence of output

--- augment.py
+++ augment.py
@@ -7,6 +7,7 @@ Copyright (c) 2026
 
 import argparse
 import os
+from pathlib import Path
 
 import dspy
 import pandas as pd
@@ -36,46 +37,47 @@ if __name__ == "__main__":
     batch_size = os.getenv("BATCH_SIZE")
     num_subdatasets = os.getenv("NUM_SUBDATASETS")
 
-    llm = dspy.LM(
-        llm_name,
-        api_key=api_key,
-        max_tokens=5_000,
-        temperature=1,
-        cache=False,
-        verbose=False,
-    )
-    dspy.configure(lm=llm)
+    if not Path(output_path).exists():
+        llm = dspy.LM(
+            llm_name,
+            api_key=api_key,
+            max_tokens=5_000,
+            temperature=1,
+            cache=False,
+            verbose=False,
+        )
+        dspy.configure(lm=llm)
 
-    if batch_size is None:
-        questions, unpacked_terms, answers, embeddings = extract(
-            dataset_path,
-            question_field,
-            answer_field,
-            repo_word_model,
-            word_model_name,
-            local_dataset,
-        )
-    else:
-        questions, unpacked_terms, answers, embeddings = extract(
-            dataset_path,
-            question_field,
-            answer_field,
-            repo_word_model,
-            word_model_name,
-            local_dataset,
-            int(batch_size),
-        )
-    new_terms = propose_terms(unpacked_terms, embeddings)
-    new_dataset = recommend(questions, new_terms, answers)
-    old_dataset = dict(zip(questions, answers))
-    final_dataset = {**old_dataset, **new_dataset}
+        if batch_size is None:
+            questions, unpacked_terms, answers, embeddings = extract(
+                dataset_path,
+                question_field,
+                answer_field,
+                repo_word_model,
+                word_model_name,
+                local_dataset,
+            )
+        else:
+            questions, unpacked_terms, answers, embeddings = extract(
+                dataset_path,
+                question_field,
+                answer_field,
+                repo_word_model,
+                word_model_name,
+                local_dataset,
+                int(batch_size),
+            )
+        new_terms = propose_terms(unpacked_terms, embeddings)
+        new_dataset = recommend(questions, new_terms, answers)
+        old_dataset = dict(zip(questions, answers))
+        final_dataset = {**old_dataset, **new_dataset}
 
-    if batch_size is not None and num_datasets is not None:
-        new_datasets = categorize_dataset(
-            final_dataset, int(num_subdatasets), int(batch_size)
-        )
-    else:
-        new_datasets = categorize_dataset(final_dataset)
-    for ind, dataset in enumerate(new_datasets):
-        df = pd.DataFrame(dataset, columns=["question", "answer"])
-        df.to_csv(f"{output_path}/set{ind}.csv", header=True)
+        if batch_size is not None and num_datasets is not None:
+            new_datasets = categorize_dataset(
+                final_dataset, int(num_subdatasets), int(batch_size)
+            )
+        else:
+            new_datasets = categorize_dataset(final_dataset)
+        for ind, dataset in enumerate(new_datasets):
+            df = pd.DataFrame(dataset, columns=["question", "answer"])
+            df.to_csv(f"{output_path}/set{ind}.csv", header=True)
